34c889b5c567814bf0ef29003840c108cb998261
Eric Foster <elitheroyalone@gmail.comgit config --global user.name eric-fostergit config --global user.email elitheroyalone@gmail.com>
2019-12-06 12:11:36 -0600

    MVP

--- client/src/App.js
+++ client/src/App.js
@@ -1,54 +1,62 @@
 import React from 'react';
+import useDarkMode from './Hooks/useDarkMode'
+
 import './App.css';
 
-class App extends React.Component() {
+class App extends React.Component {
   constructor() {
-    super()
+    super();
     this.state = {
       players: [],
-    }
-  }
+      
+    };
 
-  componentDidMount() {
-    fetch('http://localhost:500/api/players')
-    .then(res=> res.json())
-    .then(data => this.setState({players: data}))
-  }
-
-  componentDidUpdate() {
-    console.log(this.state);  
     }
 
+    componentDidMount() {
 
-  render(){
+      fetch('http://localhost:5000/api/players')
+      .then(res => res.json())
+      .then(data => this.setState( {players: data} ));
 
-    return (
-      <div className="App">
+      }
 
-        <Athletes players = {this.state.players}/>
+
+      componentDidUpdate() {
+        console.log(this.state);  
+        }
+
+        render() {
+    
+ 
+          return (
+            <div className="App">
+            
+            <Athletes players= {this.state.players}  />
+            
+            </div>
+          );
+        }
+        }
+
+        function Athletes (props) { 
+          const [darkMode, setDarkMode] = useDarkMode(false);
+         const toggleMode = e => {
+           e.preventDefault();
+           setDarkMode(!darkMode);
+         }
+         return (
+           <div>
+       
         
-      </div>
-    );
-  }
-  
-}
-
-function Athletes (props) { 
-  const [darkMode, setDarkMode] = useDarkMode(false);
- const toggleMode = e => {
-   e.preventDefault();
-   setDarkMode(!darkMode);
- }
- return (
-   <div>
+        <button onClick={toggleMode}>Custom Hook</button>
+       {props.players.map(player => (<div key={player.id}>{player.id} {player.name} {player.country} {player.searches}</div>))}
+        
+           </div>
+         );
+       }
 
 
-<button onClick={toggleMode}>Custom Hook</button>
-{props.players.map(player => (<div key={player.id}>{player.id} {player.name} {player.country} {player.searches}</div>))}
-
-   </div>
- );
-}
 
 
 export default App;
